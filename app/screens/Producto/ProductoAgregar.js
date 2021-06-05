c8042ca65c2443bb0e3c8ff06104b139bc0a5561
iserm-git <58581611+iserm-git@users.noreply.github.com>
2021-06-05 09:28:54 -0500

    Exposición realizada en clase corresponde a app-expo5-1

--- app/screens/Producto/ProductoAgregar.js
+++ app/screens/Producto/ProductoAgregar.js
@@ -1,63 +1,12 @@
-import React from 'react';
-import { StyleSheet, View, Text } from "react-native";
+import React, { useState } from 'react';
+import { Alert, Modal, Pressable, StyleSheet, View, Text } from "react-native";
 import { Button, Icon, Input } from 'react-native-elements';
+import ProductoFormulario from "./../../components/Producto/ProductoForm"
+
 
 export default function ProductoAgregar() {
-    return (
-        <View>
-            <Input
-                placeholder='Cve. producto'
-                leftIcon={{ type: 'material-community', name: 'key' }}
-            />
-            <Input
-                placeholder='Nombre producto'
-                leftIcon={{ type: 'material-community', name: 'cart' }}
-            />
-            <Input
-                placeholder='Precio'
-                leftIcon={{ type: 'material-community', name: 'currency-usd' }}
-            />
-            <Input
-                placeholder='Cantidad'
-                leftIcon={{ type: 'material-community', name: 'format-list-bulleted-square' }}
-            />
-            <Button 
-                title="Agregar producto"
-                buttonStyle={styles.btnStyle}
-                containerStyle={styles.btnContainer}
-            />
-        </View>
-    );
+
+  <ProductoFormulario />
+
 }
 
-const styles = StyleSheet.create({
-    viewBody: {
-      marginLeft: 30,
-      marginRight: 30
-    },
-    textTitle: {
-      marginTop: 20,
-      marginBottom: 20,
-      textAlign: "center",
-      fontSize: 30,
-      fontWeight: "bold"
-    },
-    btnStyle: {
-      backgroundColor: "#00a680"
-    },
-    btnContainer: {
-      width: "100%",
-      marginBottom: 5,
-      marginTop: 5
-    },
-    viewBtn: {
-      flex: 6,
-      alignItems: "center",
-    },
-    photo: {
-        height: 180,
-        width: 180,
-        marginBottom: 20,
-        marginTop: 20
-      }
-  });
